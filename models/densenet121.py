a6b5cd6449d5c5248eda9d5ca57ef37984ced181
Eyiara Oladipo <oladipea@ececs.udmercy.edu>
2026-03-24 20:44:52 -0400

    hi

--- models/densenet121.py
+++ models/densenet121.py
@@ -316,3 +316,16 @@ if __name__ == "__main__":
 # ────────────────────────────────────────────────────────────
 
 # SPURIOUS = FALSE
+
+#   Train loss: 0.4088 | Val loss: 0.4093 | Val acc: 0.7980 | Val precision: 0.8110 | Val recall: 0.7290 | Val f1: 0.7678 | Val AUC: 0.8713
+
+# Loading best checkpoint for test evaluation …
+
+# ────────────────────────────────────────────────────────────
+#   Test loss:  0.6866
+#   Accuracy:   0.6446
+#   Precision:  0.7619
+#   Recall:     0.3507
+#   F1:         0.4803
+#   AUC:        0.7440
+# ────────────────────────────────────────────────────────────
