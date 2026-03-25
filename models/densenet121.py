50412cade82527dc8062098b0e7d7f6215c3a3ed
Eyiara Oladipo <oladipea@ececs.udmercy.edu>
2026-03-24 20:41:00 -0400

    dens

--- models/densenet121.py
+++ models/densenet121.py
@@ -75,7 +75,7 @@ class DenseNet121(nn.Module):
 
         # Initial conv
         self.stem = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
