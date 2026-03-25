3c6a83e295cc18dca408d8959b448bcc9e74864e
Ara-O <oladipoeyiara@gmail.com>
2026-03-24 20:38:16 -0400

    densenet

--- models/densenet121.py
+++ models/densenet121.py
@@ -75,7 +75,7 @@ class DenseNet121(nn.Module):
 
         # Initial conv
         self.stem = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
@@ -196,7 +196,7 @@ if __name__ == "__main__":
     SEED = 10
     set_seed(SEED)
 
-    SPURIOUS = True
+    SPURIOUS = False
     BATCH_SIZE   = 32
     EPOCHS       = 15
     LR           = 1e-4    
@@ -300,3 +300,19 @@ if __name__ == "__main__":
         f"{'─'*60}"
     )
 
+# SPURIOUS = TRUE
+
+#   Train loss: 0.4088 | Val loss: 0.4093 | Val acc: 0.7980 | Val precision: 0.8110 | Val recall: 0.7290 | Val f1: 0.7678 | Val AUC: 0.8713
+
+# Loading best checkpoint for test evaluation …
+
+# ────────────────────────────────────────────────────────────
+#   Test loss:  0.6866
+#   Accuracy:   0.6446
+#   Precision:  0.7619
+#   Recall:     0.3507
+#   F1:         0.4803
+#   AUC:        0.7440
+# ────────────────────────────────────────────────────────────
+
+# SPURIOUS = FALSE
